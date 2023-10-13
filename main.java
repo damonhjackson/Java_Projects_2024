5616c3c02f63cc5d4ef6b58dd13d9ede17744dcc
Damon Jackson <78815594+damonhjackson@users.noreply.github.com>
2023-10-12 21:12:45 -0500

    Variable practice, Int, long, float, double, boolean, char

---
+++ main.java
@@ -0,0 +1,20 @@
+public class main {
+    public static void main(String[] args) {
+        int x = 123; //initialization
+        long y = 123456789;
+        float f = 3.14f;
+        double d = 3.14;
+        boolean b = true;
+        char s = '@';
+        String name = "Damon";
+
+        System.out.println("My number is x: " + x);
+        System.out.println("long number: " + y);
+        System.out.println("float pie: " + f);
+        System.out.println("double pie: " + d);
+        System.out.println("boolean true: " + b);
+        System.out.println("char symbol: " + s);
+        System.out.println("Hello" + name);
+
+    }
+}
